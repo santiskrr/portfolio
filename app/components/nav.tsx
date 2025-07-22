84b466620cef035e258af51099087735a1f203aa
santiskrr <purpleblacksouls@gmail.com>
2025-07-21 21:53:07 -0300

    texto cursor

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,10 +14,10 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] border border-red-500 rounded-l mb-16">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className=""
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
-
+import TextType from './components/TextType';
 
 export default function Page() {
   return (
+    <>
+    <TextType 
+  text={["Mi PortFolioooooooooooo"]}
+  typingSpeed={75}
+  pauseDuration={1500}
+  showCursor={true}
+  cursorCharacter="|"
+/>
+
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Mi Portfolio
+  
       </h1>
       <p className="mb-4">
         {`no se.`}
       </p>
-      <div className="my-8">
-      </div>
+         <TextType 
+  text={["Mi PortFolioooooooooooo"]}
+  typingSpeed={75}
+  pauseDuration={1500}
+  showCursor={true}
+  cursorCharacter="|"
+/>
     </section>
+    </>
+
   )
 }
