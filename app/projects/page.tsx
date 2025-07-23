d4b730298217bd64ab680bc47ac8acc1c625e51c
santiskrr <purpleblacksouls@gmail.com>
2025-07-22 21:40:30 -0300

    infinitemenu

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,31 +1,37 @@
-import ScrollStack, { ScrollStackItem } from '../components/ScrollStack';
+import InfiniteMenu from '../components/InfiniteMenu'
+
 
 export default function Page() {
+  const items = [
+    {
+      image: 'https://fptpagsqdrwmvzonuhsd.supabase.co/storage/v1/object/public/imagenes//logo_toroswok.svg',
+      link: 'https://toroswok-santiskrrs-projects.vercel.app/',
+      title: 'Item 1',
+      description: ''
+    },
+    {
+      image: 'https://picsum.photos/400/400?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 2',
+      description: 'This is pretty cool, right?'
+    },
+    {
+      image: 'https://picsum.photos/500/500?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 3',
+      description: 'This is pretty cool, right?'
+    },
+    {
+      image: 'https://picsum.photos/600/600?grayscale',
+      link: 'https://google.com/',
+      title: 'Item 4',
+      description: 'This is pretty cool, right?'
+    }
+  ];
+
   return (
-    <>
-    <div>
-<ScrollStack>
-  <ScrollStackItem>
-    <h2>Toro's wok</h2>
-    <p>
-      <a href="https://toroswok-santiskrrs-projects.vercel.app/" target="_blank" rel="noopener noreferrer">
-        Visit Toro's wok project
-      </a>
-    </p>
-  </ScrollStackItem>
-  <ScrollStackItem>
-    <h2>Card 2</h2>
-    <p>This is the second card in the stack</p>
-  </ScrollStackItem>
-  <ScrollStackItem>
-    <h2>Card 3</h2>
-    <p>This is the third card in the stack</p>
-  </ScrollStackItem>
-</ScrollStack>
-        
-
-
+    <div style={{ height: '500px', position: 'relative' }}>
+      <InfiniteMenu items={items} />
     </div>
-    </>
-  )
+  );
 }
