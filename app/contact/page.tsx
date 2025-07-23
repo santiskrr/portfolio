faef9b7855574b08c67e3412c8cb1965bf61a7ff
santiskrr <purpleblacksouls@gmail.com>
2025-07-22 21:13:59 -0300

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,12 @@
 "use client"
-import MagicBento from '../components/MagicBento'
+
 
 
 export default function Page() {
   return (
     <>
     <div>
-        <MagicBento 
-            textAutoHide={true}
-            enableStars={true}
-            enableSpotlight={true}
-            enableBorderGlow={true}
-            enableTilt={true}
-            enableMagnetism={true}
-            clickEffect={true}
-            spotlightRadius={300}
-            particleCount={12}
-            glowColor="132, 0, 255"
-            />
+       
 
     </div>
     
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,27 @@
+import ScrollStack, { ScrollStackItem } from '../components/ScrollStack';
+
 export default function Page() {
   return (
     <>
-    <div className= "bg-black h-100 w-100">
-        <h1 className="text-center">holaaaaaaa</h1>
+    <div>
+<ScrollStack>
+  <ScrollStackItem>
+    <h2>Toro's wok</h2>
+    <p>
+      <a href="https://toroswok-santiskrrs-projects.vercel.app/" target="_blank" rel="noopener noreferrer">
+        Visit Toro's wok project
+      </a>
+    </p>
+  </ScrollStackItem>
+  <ScrollStackItem>
+    <h2>Card 2</h2>
+    <p>This is the second card in the stack</p>
+  </ScrollStackItem>
+  <ScrollStackItem>
+    <h2>Card 3</h2>
+    <p>This is the third card in the stack</p>
+  </ScrollStackItem>
+</ScrollStack>
         
 
 
